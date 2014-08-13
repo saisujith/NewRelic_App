30b0fd17bffc3da1362af81a64e65ae5e274ad69
saisujith <saisujith717@gmail.com>
2014-08-12 20:33:43 -0700

    removed sqlite3 for production mode

--- app/controllers/emailer_controller.rb
+++ app/controllers/emailer_controller.rb
@@ -2,7 +2,7 @@ class EmailerController < ApplicationController
 
   def get_send_mail
     url = request.original_url
-	RestClient.post "#{url}send_mail", {to: 'saisujith717@gmail.com', subject: 'hello world', body: 'test mail body'}.to_json, accept: :json, content_type: json
+	RestClient.post "#{url}send_mail", {to: 'saisujith717@gmail.com', subject: 'hello world', body: 'test mail body'}.to_json, accept: :json, content_type: :json
   end
   
   def send_mail
